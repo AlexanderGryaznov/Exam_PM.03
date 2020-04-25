c5cce4462e482f8baf8e75dedae7084a49cd2ab2
altarnik33 <stgeorgy33@mail.ru>
2020-04-25 09:15:39 +0300

    Создал клас Smatphone и конструктор.

---
+++ src/Smartphone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Smartphone
+    {
+        public string Brand = "";
+        public string Model = "";
+        public double Price = 0;
+
+        public Smartphone(string brand, string model, double price)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Price = price;
+        }
+
+    }
+}
