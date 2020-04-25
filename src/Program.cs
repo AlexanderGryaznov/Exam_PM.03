8bb8b0e69198b4ae1c5defc5744a7333524d6613
altarnik33 <stgeorgy33@mail.ru>
2020-04-25 11:58:28 +0300

    Отредактирована сортировка и добавлен метод сохранения в файл - Save_File().

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,17 +15,20 @@ namespace src
         {
             Array();
             sorting();
+            Save_file();
             Console.ReadKey();
         }
 
         private static void Array()
         {
-            Console.WriteLine("Какое количество смартфонов Вы хотите заполнить:");
-            N = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Какое количество смартфонов Вы хотите заполнить:");
+                N = Convert.ToInt32(Console.ReadLine());
                 for (int i = 0; i < N; i++)
                 {
-                Console.WriteLine("Заполните информацию о" + " " + (i + 1) + "-м " + "смартфоне:");
-                Smartphone TempPhones = new Smartphone();
+                    Console.WriteLine("Заполните информацию о" + " " + (i + 1) + "-м " + "смартфоне:");
+                    Smartphone TempPhones = new Smartphone();
                     Console.WriteLine("Марка:");
                     TempPhones.Brand = Console.ReadLine();
 
@@ -34,12 +38,26 @@ namespace src
                     Console.WriteLine("Цена:");
                     TempPhones.Price = Convert.ToDouble(Console.ReadLine());
 
-                Phones.Add(TempPhones);
+                    Phones.Add(TempPhones);
                 }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вы ввели не число!");
+            }
         }
         private static void sorting()
         {
-            Phones.OrderBy(r => r.Brand).ThenBy(r => r.Price).ToArray();
+            Phones.Sort((a,b)=>string.Compare(a.Brand, b.Price.ToString()));
+        }
+
+        static public void Save_file()
+        {
+            using (StreamWriter s = new StreamWriter("Smartphone.txt"))
+            {
+                foreach (Smartphone sp in Phones)
+                    s.WriteLine(sp.Brand + ", " + sp.Model + ", " + sp.Price.ToString());
+            }
         }
     }
 }
