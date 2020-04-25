7101331a078e5e7cb2341a4b3cfa2242ee397fdb
altarnik33 <stgeorgy33@mail.ru>
2020-04-25 11:04:10 +0300

    Создание метода Array(), который заполняет массив

--- src/Program.cs
+++ src/Program.cs
@@ -8,9 +8,33 @@ namespace src
 {
     class Program
     {
+        static public int N;
+        static public List<Smartphone> Phones = new List<Smartphone>();
         static void Main(string[] args)
         {
-            
+            Array();
+            Console.ReadKey();
+        }
+
+        private static void Array()
+        {
+            Console.WriteLine("Какое количество смартфонов Вы хотите заполнить:");
+            N = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < N; i++)
+                {
+                Console.WriteLine("Заполните информацию о" + " " + (i + 1) + "-м " + "смартфоне:");
+                Smartphone TempPhones = new Smartphone();
+                    Console.WriteLine("Марка:");
+                    TempPhones.Brand = Console.ReadLine();
+
+                    Console.WriteLine("Модель:");
+                    TempPhones.Model = Console.ReadLine();
+
+                    Console.WriteLine("Цена:");
+                    TempPhones.Price = Convert.ToDouble(Console.ReadLine());
+
+                Phones.Add(TempPhones);
+                }
         }
     }
 }
